8471e3398d2d3273fc45048e6b4cc6b7457c43f9
neochildish <neochildish@outlook.com>
2024-02-10 06:51:55 -0600

    init commit

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+"""Defines the url patterns for the 'main' app"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+app_name = 'main'
+urlpatterns = [
+    path('', views.index)
+]
